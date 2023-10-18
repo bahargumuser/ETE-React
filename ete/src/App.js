d79f044409f1ab77e95c15395a3cdac4e4ab5c99
Bahar Gümüşer <121288876+bahargumuser@users.noreply.github.com>
2023-10-18 23:44:49 +0300

    add login and register

---
+++ ete/src/App.js
@@ -0,0 +1,26 @@
+import React, {useState} from 'react';
+import { Routes, Route } from 'react-router-dom';
+import './App.css';
+import { Login } from './Login';
+import { Register } from './Register';
+import { Homepage } from "./Homepage";
+
+function App() {
+  const [currentForm, setCurrentForm] = useState('login');
+
+const toggleForm = (forName) => {
+  setCurrentForm(forName);
+}
+
+  return (
+    <div className="App">
+      {
+        currentForm === "login" ? <Login onFormSwitch={toggleForm}/> : <Register onFormSwitch={toggleForm}/>
+      }
+  
+
+    </div>
+  );
+}
+
+export default App;
