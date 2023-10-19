be1c76adaf0d7b027012f852dfeb4ce6166209fd
Bahar Gümüşer <121288876+bahargumuser@users.noreply.github.com>
2023-10-19 05:53:43 +0300

    add Homepage router

--- ete/src/Login.js
+++ ete/src/Login.js
@@ -1,31 +1,32 @@
 import React, {useState} from "react";
-
+import Homepage from "./Homepage";
+import { useHistory } from "react-router-dom";
 
 export const Login = (props) => {
     const [email, setEmail] = useState('');
     const [pass, setPass] = useState('');
+    let history = useHistory();
 
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        console.log(email);
-  
+        history.push("/Homepage");
     }
-
    
 
        return (
+   
         <div className="auth-form">
-            <h2>Login</h2>
-      <form className="login-form" onSubmit={handleSubmit}>
+        <h2>Login</h2>
+        <form className="login-form" onSubmit={handleSubmit}>
         <label htmlFor="email">email</label>
         <input value={email} onChange={(e) => setEmail(e.target.value)}type="email" placeholder="youremail@gmail.com" id="email" name="email" />
         <label htmlFor="password">password</label>
         <input value={pass} onChange={(e) => setPass(e.target.value)}type="password" placeholder="******" id="password" name="password"></input>
         <button type="submit">Login</button>
-      </form>
-      <button className="link-btn" onClick={() => props.onFormSwitch('register')}>Don't have an account? Register here.</button>
-      </div>
-  
+        </form>
+        <button className="link-btn" onClick={() => props.onFormSwitch('register')}>Don't have an account? Register here.</button>
+        </div>
     )
 }
+export default Login;
