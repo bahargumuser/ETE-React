88b83acdfe19b68bd2b51c54db15daacf0d5e70c
Bahar Gümüşer <121288876+bahargumuser@users.noreply.github.com>
2023-10-20 03:34:00 +0300

    create homepage

--- ete/src/Homepage.js
+++ ete/src/Homepage.js
@@ -1,11 +1,10 @@
 import React, { useState } from "react";
-import { useHistory } from "react-router-dom";
+import { useHistory, Link } from "react-router-dom";
 import Companies from './Companies';
 import Products from './Products';
 import { Route } from "react-router-dom";
 
 function Homepage() {
-  const [companies, setCompanies] = useState('')
 
   let history = useHistory();
 
@@ -13,15 +12,14 @@ function Homepage() {
     e.preventDefault();
     history.push("/Companies");
 }
-
-
     return (
       <div className="Homepage" onSubmit={handleChange}>
-    
-     
+
         <h1 className='topic'>Statistic</h1>
-        <button type="submit">Companies</button>
-        <button type="submit">Products</button>
+        
+        <Link to="/Companies">Companies</Link>
+        <Link to="/Products">Products</Link>
+        
   
       </div>
   
