2ac589e4de237b752fd921464af2e56b2962ca3d
Bahar Gümüşer <121288876+bahargumuser@users.noreply.github.com>
2023-10-22 00:17:22 +0300

    add useEffect

--- ete/src/Companies.js
+++ ete/src/Companies.js
@@ -1,15 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from 'react';
 import { BsFillTrashFill, BsFillPencilFill } from "react-icons/bs";
+import axios from 'axios';
 
+function Companies  ({rows, deleteRow, editRow})  {
+    const [companiesData, setCompaniesData] = useState([]);
 
-function Companies ({rows, deleteRow, editRow})  {
-    return (
+    useEffect(() => {
+        axios.get('/companies')
+          .then((response) => {
+            setCompaniesData(response.data);
+          })
+          .catch((error) => {
+            console.error('Veri çekme hatası:', error);
+          });
+      }, []);
+
+      return (
         <div className="Companies">
             <table className="table">
                 <thead>
                     <tr>
-                        <th>Page</th>
-                        <th className="expand">Description</th>
+                        <th>Company Name</th>
+                        <th>Legal Number</th>
+                        <th>Incorporation Country</th>
+                        <th>Website</th>
                         <th>Status</th>
                         <th>Actions</th>
                     </tr>
@@ -17,12 +31,14 @@ function Companies ({rows, deleteRow, editRow})  {
                 <tbody>
                     {rows.map((row, idx) => {
                         const statusText =
-                        row.status.charAt(0).toUpperCase() + row.status.slice(1);
-
-                        return(
+                            row.status.charAt(0).toUpperCase() + row.status.slice(1);
+    
+                        return (
                             <tr key={idx}>
-                                <td>{row.page}</td>
-                                <td className="expand">{row.description}</td>
+                                <td>{row.companyName}</td>
+                                <td>{row.legalNumber}</td>
+                                <td>{row.incorporationLegalNumber}</td>
+                                <td>{row.website}</td>
                                 <td>
                                     <span className={`label label-${row.status}`}>
                                         {statusText}
@@ -31,24 +47,22 @@ function Companies ({rows, deleteRow, editRow})  {
                                 <td className="fit">
                                     <span className="actions">
                                         <BsFillTrashFill
-                                        className="delete-btn"
-                                        onClick={() => deleteRow(idx)}
+                                            className="delete-btn"
+                                            onClick={() => deleteRow(idx)}
                                         />
                                         <BsFillPencilFill
-                                        className="edit-btn"
-                                        onClick={() => editRow(idx)}
+                                            className="edit-btn"
+                                            onClick={() => editRow(idx)}
                                         />
                                     </span>
                                 </td>
                             </tr>
-
-                        )
+                        );
                     })}
                 </tbody>
             </table>
-
         </div>
-    )
+    );
 }
 
 export default Companies;
