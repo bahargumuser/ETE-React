8056a7b52ea4250dffcf1e6a75d721f27ae55d2c
Bahar Gümüşer <121288876+bahargumuser@users.noreply.github.com>
2023-10-23 07:48:03 +0300

    jwt connect

--- ete/src/Login.js
+++ ete/src/Login.js
@@ -1,29 +1,68 @@
-import React, {useState} from "react";
+import axios from "axios";
+import React, { useState } from "react";
 import { useHistory } from "react-router-dom";
+import jwt_decode from "jwt-decode";
+ 
+
 
 export const Login = (props) => {
-    const [email, setEmail] = useState('');
-    const [pass, setPass] = useState('');
-    let history = useHistory();
+  const [data, setData] = useState({
+    email: "",
+    password: "",
+  });
+  const [error,setError]=useState("");
 
+  let history = useHistory();
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    axios
+      .post("http://localhost:9000/users/login", data)
+      .then((response) => {
+       
+        console.log("Login successful:", response.data);
+        localStorage.setItem("token",response.data.token);
+        let user= jwt_decode(response.data.token);
+        const userJSON = JSON.stringify(user);
+      
+        localStorage.setItem("user",userJSON)
         history.push("/Homepage");
-    }
-       return (
-   
-        <div className="auth-form">
-        <h2>Login</h2>
-        <form className="login-form" onSubmit={handleSubmit}>
+      })
+      .catch((error) => {
+        setError(error.response.data.message)
+
+      });
+  };
+
+  return (
+    <div className="auth-form">
+      <h2>Login</h2>
+      {error && <h2>{error}</h2>}
+      <form className="login-form" onSubmit={handleSubmit}>
         <label htmlFor="email">E-mail</label>
-        <input value={email} onChange={(e) => setEmail(e.target.value)}type="email" placeholder="youremail@gmail.com" id="email" name="email" />
+        <input
+          onChange={(e) => setData({ ...data, email: e.target.value })}
+          type="email"
+          placeholder="youremail@gmail.com"
+          id="email"
+          name="email"
+        />
+        
         <label htmlFor="password">Password</label>
-        <input value={pass} onChange={(e) => setPass(e.target.value)}type="password" placeholder="******" id="password" name="password"></input>
+        <input
+          onChange={(e) => setData({ ...data, password: e.target.value })}
+          type="password"
+          placeholder="******"
+          id="password"
+          name="password"
+        ></input>
         <button type="submit">Login</button>
-        </form>
-        <button className="link-btn" onClick={() => props.onFormSwitch('register')}>Don't have an account? Register here.</button>
-        </div>
-    )
-}
+      </form>
+      <button className="link-btn" onClick={() => props.onFormSwitch("register")}>
+        Don't have an account? Register here.
+      </button>
+    </div>
+  );
+};
 export default Login;
--- ete/src/Register.js
+++ ete/src/Register.js
@@ -1,32 +1,50 @@
 import React, {useState} from "react";
-import Homepage from "./Homepage";
 import { useHistory } from "react-router-dom";
+import axios from "axios";
 
 
 export const Register = (props) => {
-    const [email, setEmail] = useState('');
-    const [pass, setPass] = useState('');
-    const [name, setName] = useState('');
+  
     let history = useHistory();
 
+    const [data, setData] = useState({
+      email: "",
+      password: "",
+      fullname:""
+    });
+    const [error,setError]=useState("");
 
-
+  
     const handleSubmit = (e) => {
-        e.preventDefault();
-        history.push("/Homepage");
-    }
+      e.preventDefault();
+  
+      axios
+        .post("http://localhost:9000/users/register", data)
+        .then((response) => {
+         
+  
+        console.log(response.data)
+          history.push("/Homepage");
+        })
+        .catch((error) => {
+          setError(error.response.data.message)
+  
+        });
+    };
+  
 
     return (
   
         <div className="auth-form">
             <h2>Login</h2>
+            {error&& <h2>{error}</h2>}
       <form className="register-form" onSubmit={handleSubmit}>
         <label htmlFor="name">Full Name</label>
-        <input value={name} name="name" onChange={(e) => setName(e.target.value)} id="name" placeholder="Full Name" />
+        <input name="name" onChange={(e) => setData({...data,fullname:e.target.value})} id="fullname" placeholder="Full Name" />
         <label htmlFor="email">E-mail</label>
-        <input value={email} onChange={(e) => setEmail(e.target.value)}type="email" placeholder="youremail@gmail.com" id="email" name="email" />
+        <input  onChange={(e) => setData({...data,email:e.target.value})}type="email" placeholder="youremail@gmail.com" id="email" name="email" />
         <label htmlFor="password"></label>
-        <input value={pass} onChange={(e) => setPass(e.target.value)}type="password" placeholder="******" id="password" name="password"></input>
+        <input onChange={(e) => setData({...data,password:e.target.value})}type="password" placeholder="******" id="password" name="password"></input>
         <button type="submit">Login</button>
       </form>
       <button className="link-btn" onClick={() => props.onFormSwitch('login')}>Already have an account? Login here.</button>
